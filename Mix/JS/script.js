9d3d5b2e15e6ff2846c284b2249a68a4dfcc8980
Criz <crizmo595@gmail.com>
2022-03-04 10:51:52 +0530

    Create script.js

---
+++ Mix/JS/script.js
@@ -0,0 +1,25 @@
+function display() {
+    document.write("Hello World");
+}
+
+function add(a, b) {
+
+    const c = a + b;
+    document.write("Total of a & b: " + c);
+}
+
+function area(length, width) {
+
+    const area = length * width;
+    document.write("Area of rectangle: " + area);
+
+}
+
+function back() {
+    const button = document.createElement("button");
+    button.innerHTML = "Return to Main Page";
+    button.onclick = function () {
+        window.location.href = "../JS/start.html";
+    }
+    document.body.appendChild(button);
+}
